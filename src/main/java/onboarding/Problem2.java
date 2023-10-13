3b0e8415a0ba68a54985cbd2346111b01f4d27d1
leesangjun <hubls@naver.com>
2023-10-14 00:49:14 +0900

    feat(Problem2): 연속된 문자열을 삭제하는 기능 구현

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,36 @@
 package onboarding;
 
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = decryptCryptogram(cryptogram);
         return answer;
+    }
+
+    public static String decryptCryptogram(String cryptogram) {
+        int count = 0;
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            if (cryptogram.charAt(i) != cryptogram.charAt(i + 1)) {
+                count += 1;
+            }
+            if (count == cryptogram.length() - 1) {
+                return cryptogram;
+            }
+        }
+
+        for (int i = 0; i < cryptogram.length() - 1; i ++) {
+
+            if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
+
+                int j = i;
+                while ((cryptogram.charAt(i) == cryptogram.charAt(j)) && (j < cryptogram.length() - 1))  {
+                    j += 1;
+                }
+
+                String trimCryptogram = cryptogram.substring(0, i) + cryptogram.substring(j, cryptogram.length());
+                return decryptCryptogram(trimCryptogram);
+            }
+        }
+        return "";
     }
 }
