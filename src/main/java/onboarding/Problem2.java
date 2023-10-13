bb28c05e80c78819d928111e55a954d64d845fc1
leesangjun <hubls@naver.com>
2023-10-14 01:20:46 +0900

    Refactor(Problem2): 연속된 문자의 끝 인덱스를 구하는 기능 메서드로 분리

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,27 +7,31 @@ public class Problem2 {
         return answer;
     }
 
+    public static int getSecondTrim(String cryptogram, int firstTrim) {
+        int secondTrim = firstTrim + 1;
+        while ((cryptogram.charAt(firstTrim) == cryptogram.charAt(secondTrim)) && (secondTrim < cryptogram.length() - 1)) {
+            secondTrim += 1;
+        }
+        return secondTrim;
+    }
+
     public static String decryptCryptogram(String cryptogram) {
-        int count = 0;
-        for (int i = 0; i < cryptogram.length() - 1; i++) {
+        int stringCount = 0;
+        int cryptogramLength = cryptogram.length();
+
+        for (int i = 0; i < cryptogramLength - 1; i++) {
             if (cryptogram.charAt(i) != cryptogram.charAt(i + 1)) {
-                count += 1;
+                stringCount += 1;
             }
-            if (count == cryptogram.length() - 1) {
+            if (stringCount == cryptogram.length() - 1) {
                 return cryptogram;
             }
         }
 
-        for (int i = 0; i < cryptogram.length() - 1; i ++) {
-
-            if (cryptogram.charAt(i) == cryptogram.charAt(i + 1)) {
-
-                int j = i;
-                while ((cryptogram.charAt(i) == cryptogram.charAt(j)) && (j < cryptogram.length() - 1))  {
-                    j += 1;
-                }
-
-                String trimCryptogram = cryptogram.substring(0, i) + cryptogram.substring(j, cryptogram.length());
+        for (int firstTrim = 0; firstTrim < cryptogramLength - 1; firstTrim ++) {
+            if (cryptogram.charAt(firstTrim) == cryptogram.charAt(firstTrim + 1)) {
+                int secondTrim = getSecondTrim(cryptogram, firstTrim);
+                String trimCryptogram = cryptogram.substring(0, firstTrim) + cryptogram.substring(secondTrim, cryptogramLength);
                 return decryptCryptogram(trimCryptogram);
             }
         }
