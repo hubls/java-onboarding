3a153e397b1ee91be18b2900e263afbc2d8038dc
leesangjun <hubls@naver.com>
2023-10-14 23:28:23 +0900

    feat(Problem3): 입력받은 수까지 손뼉을 몇 번 쳐야 하는지 횟수를 리턴하는 기능 구현

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,32 @@
 package onboarding;
-
 public class Problem3 {
+    static final char THREE = '3';
+    static final char SIX = '6';
+    static final char NINE = '9';
     public static int solution(int number) {
         int answer = 0;
+        answer = countClap(number);
         return answer;
+    }
+
+    public static int countClap(int number) {
+        int count = 0;
+        while (number != 0) {
+            String numberAsString = Integer.toString(number);
+
+            for (int i = 0; i < numberAsString.length(); i++) {
+                if (numberAsString.charAt(i) == THREE) {
+                    count += 1;
+                }
+                if (numberAsString.charAt(i) == SIX) {
+                    count += 1;
+                }
+                if (numberAsString.charAt(i) == NINE) {
+                    count += 1;
+                }
+            }
+            number--;
+        }
+        return count;
     }
 }
