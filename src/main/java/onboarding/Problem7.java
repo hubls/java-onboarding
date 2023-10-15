04b1bf16d7db675a7882d18a8e7303e70aebe5b0
leesangjun <hubls@naver.com>
2023-10-15 13:48:46 +0900

    feat(Problem7): 사용자와 친구인 사람들을 구하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,26 @@
 package onboarding;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
+    }
+
+    private static List<String> userFriendsList(String user, List<List<String>> friends) {
+        List<String> userFriends = new ArrayList<>();
+        for (int i = 0; i < friends.size(); i++) {
+            List<String> friendRelationship = friends.get(i);
+
+            if (friendRelationship.get(0) == user) {
+                userFriends.add(friendRelationship.get(0));
+            }
+
+            if (friendRelationship.get(1) == user) {
+                userFriends.add(friendRelationship.get(0));
+            }
+        }
+        return userFriends;
     }
 }
