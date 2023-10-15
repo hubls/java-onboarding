93791a650b489a74d15e0025152e1c493b02b8ce
leesangjun <hubls@naver.com>
2023-10-15 13:06:58 +0900

    feat(Problem4): 특정 문자가 알파벳 대문자인지 알파벳 소문자인지 확인 후 청개구리 사전에 따라 변환하는 기능 구현

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,7 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        answer = getReverseString(word);
         return answer;
     }
 
@@ -14,5 +15,33 @@ public class Problem4 {
     private static char getReverseLowerChar(char word) {
         char reverseChar = (char) ('a' + 'z' - word);
         return reverseChar;
+    }
+
+    private static String getReverseString(String word) {
+        String reverseString = "";
+
+        for (int i = 0; i < word.length(); i++) {
+            char newChar = 0;
+            char oldChar = word.charAt(i);
+
+            if (oldChar >= 'a' && oldChar <= 'z') {
+                newChar = getReverseLowerChar(oldChar);
+            }
+
+            if (oldChar >= 'A' && oldChar <= 'Z') {
+                newChar = getReverseUpperChar(oldChar);
+            }
+
+            if (oldChar == ' ') {
+                newChar = ' ';
+            }
+
+            if (newChar == 0) {
+                newChar = oldChar;
+            }
+
+            reverseString += newChar;
+        }
+        return reverseString;
     }
 }
