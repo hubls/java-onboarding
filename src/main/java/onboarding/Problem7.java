f2110f966487755870141eb8a3d375ce0b84dd8b
leesangjun <hubls@naver.com>
2023-10-15 13:53:17 +0900

    feat(PROBLEM7): 친구의 친구는 10점, 방문자는 1점의 점수를 합산하여 내림차순으로 리스트로 반환하는 기능 구현

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -29,4 +29,46 @@ public class Problem7 {
         mutableVisitors.removeAll(userFriends);
         return mutableVisitors;
     }
+
+    private static List<String> getSortScore(HashMap<String, Integer> score) {
+        int friendSize = score.size();
+
+        if (friendSize > 5) {
+            friendSize = 5;
+        }
+
+        List<Map.Entry<String, Integer>> sortScore = new ArrayList<>(score.entrySet());
+        Collections.sort(sortScore, (entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue()));
+
+        List<String> resultScore = new ArrayList<>();
+        for (int i = 0; i < friendSize; i++) {
+            Map.Entry<String, Integer> entry = sortScore.get(i);
+            resultScore.add(String.valueOf(entry.getKey()));
+        }
+        return resultScore;
+
+    }
+
+    private static List<String> getFriendScore(List<String> friendsfriends, List<String> visitors) {
+        HashMap<String, Integer> score = new HashMap<String, Integer>();
+        for (String name : friendsfriends) {
+            if (!score.containsKey(name)) {
+                score.put(name, 10);
+            }
+            if (score.containsKey(name)) {
+                score.put(name, score.get(name) + 10);
+            }
+        }
+
+        for (String visitorName : visitors) {
+            if (!score.containsKey(visitorName)){
+                score.put(visitorName, 1);
+            }
+            if (score.containsKey(visitorName)){
+                score.put(visitorName, score.get(visitorName) + 1);
+            }
+        }
+        return getSortScore(score);
+    }
+
 }
