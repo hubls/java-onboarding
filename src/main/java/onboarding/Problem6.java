7b781184c049028516d284834f1eb13ac67ad3ff
leesangjun <hubls@naver.com>
2023-10-15 13:30:33 +0900

    feat(PROBLEM6): 자신을 제외한 크루 닉네임을 모두 더해서 포함되는지 확인하는 기능 구현

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Problem6 {
@@ -7,4 +9,32 @@ public class Problem6 {
         List<String> answer = List.of("answer");
         return answer;
     }
+
+    private static boolean duplicateCheck(List<String> crewInformation, int crewIndex, List<List<String>> forms, int theNumberOfCrew) {
+        String nicknameMerge = "";
+        int nicknameLength = crewInformation.get(1).length();
+
+        if (nicknameLength < 2) {
+            return false;
+        }
+
+        for (int i = 0; i < crewIndex; i++){
+            nicknameMerge += forms.get(i).get(1) + ' ';
+        }
+
+        for (int i = crewIndex + 1; i < theNumberOfCrew; i++) {
+            nicknameMerge += forms.get(i).get(1) + ' ';
+        }
+
+        for (int i = 0; i < nicknameLength - 1; i++) {
+            String checkName = crewInformation.get(1).substring(i, i + 2);
+
+            if (nicknameMerge.contains(checkName)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
